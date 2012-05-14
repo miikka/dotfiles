8747e78cad83b7e599aca00e3eb366951c31cc8c
Miikka Koskinen <miikka.koskinen@iki.fi>
2012-05-14 16:29:40 +0300

    xmonad.hs: urxvtc, lock screen, chromium

--- xmonad.hs
+++ xmonad.hs
@@ -4,7 +4,7 @@ import XMonad.Hooks.DynamicLog
 import XMonad.Hooks.ManageDocks
 import XMonad.Layout.Tabbed
 import XMonad.Util.EZConfig (additionalKeys)
-import XMonad.Util.Run (spawnPipe)
+import XMonad.Util.Run (safeSpawn, spawnPipe)
 import XMonad.Util.Scratchpad
 
 import qualified XMonad.StackSet as W
@@ -18,17 +18,20 @@ main = do
 	let layouts = tiled ||| Mirror tiled ||| simpleTabbed
 
 	let myManageHook = composeAll
-		[ className =? "Firefox" --> doF (W.shift "2") ]
+		[ className =? "Firefox" --> doF (W.shift "2")
+		, className =? "Chromium" --> doF (W.shift "2") ]
 
 	let config = defaultConfig
 		{ modMask = mod4Mask
 		, manageHook = manageDocks <+> scratchpadManageHookDefault <+> myManageHook <+> manageHook defaultConfig
 		, layoutHook = avoidStruts layouts
 		, logHook = dynamicLogWithPP $ xmobarPP { ppOutput = hPutStrLn xmproc }
-		, terminal = "urxvt"
+		, terminal = "urxvtc"
 		}
 
-	let	keys = [ ((mod4Mask, xK_a	), scratchpadSpawnAction config)
-			   , ((mod4Mask, xK_Return), promote)]
+	let	keys = [ ((mod4Mask, xK_a	),      scratchpadSpawnAction config)
+			   , ((mod4Mask, xK_Return),    promote)
+			   , ((mod4Mask, xK_BackSpace), safeSpawn "slock" [])
+			   ]
 	
 	xmonad $ config `additionalKeys` keys
